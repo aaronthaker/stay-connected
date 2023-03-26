f720feec51da258d9ae12fa57dbe63cc047b3f8a
Aaron <aaron.thaker105@gmail.com>
2023-03-26 22:48:20 +0100

    undo

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -38,10 +38,6 @@ export class MessagesComponent implements OnInit, OnDestroy {
     console.log('Socket connection initialized:', this.socket);
   }
 
-  getUnreadCount(userId: string): number {
-    return this.unreadMessages.filter(message => message?.senderId === userId).length;
-  }
-
   ngOnDestroy() {
     if (this.userSub) {
       this.userSub.unsubscribe();
