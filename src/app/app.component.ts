93cd44fa19aacb032bf037b5f2e447b0dc1823d4
Aaron <aaron.thaker105@gmail.com>
2023-04-01 17:07:40 +0100

    Only show side nav if logged in (need link to signup)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from './auth/auth.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
-import { NavigationEnd, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { MessagesService } from './messages/messages.service';
 import { SharedService } from './shared.service';
 
